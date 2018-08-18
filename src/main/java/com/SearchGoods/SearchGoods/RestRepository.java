a3a9e9359818e53f7f951a1d65b33a5c0cc2f9df
Ivanov Evgenii <ivanoff_john@mail.ru>
2018-08-18 20:47:23 +0300

    Initial commit

---
+++ src/main/java/com/SearchGoods/SearchGoods/RestRepository.java
@@ -0,0 +1,8 @@
+package com.SearchGoods.SearchGoods;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface RestRepository extends CrudRepository<Cars, Long> {
+}

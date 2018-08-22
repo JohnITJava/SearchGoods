6db0047325533a8a142b09904cf8ad497d55c87d
Ivanov Evgenii <ivanoff_john@mail.ru>
2018-08-22 04:56:03 +0300

    ManyToMany without controllers

--- src/main/java/com/SearchGoods/SearchGoodsApplication.java
+++ src/main/java/com/SearchGoods/SearchGoodsApplication.java
@@ -1,14 +1,51 @@
 package com.SearchGoods;
 
+import com.SearchGoods.model.Categories;
+import com.SearchGoods.model.Goods;
+import com.SearchGoods.repository.CategoriesRepository;
+import com.SearchGoods.repository.GoodsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class SearchGoodsApplication {
+public class SearchGoodsApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriesRepository categoriesRepository;
+
+	@Autowired
+	private GoodsRepository goodsRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchGoodsApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// Cleanup the tables
+		goodsRepository.deleteAllInBatch();
+		categoriesRepository.deleteAllInBatch();
+
+
+		Goods goods = new Goods("Java", "It's very common");
+
+		// Create two tags
+		Categories cat1 = new Categories("Book");
+		Categories cat2 = new Categories("Program");
+
+
+		// Add tag references in the post
+		goods.getCategories().add(cat1);
+		goods.getCategories().add(cat2);
+
+		// Add post reference in the tags
+		cat1.getGoods().add(goods);
+		cat2.getGoods().add(goods);
+
+		goodsRepository.save(goods);
 	}
 }
--- src/main/java/com/SearchGoods/model/Goods.java
+++ src/main/java/com/SearchGoods/model/Goods.java
@@ -4,6 +4,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "goods")
@@ -19,6 +21,24 @@ public class Goods{
 
     @NotBlank
     private String descr;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            })
+    @JoinTable(name = "good_cat",
+            joinColumns = { @JoinColumn(name = "good_id") },
+            inverseJoinColumns = { @JoinColumn(name = "cat_id") })
+    private Set<Categories> categories = new HashSet<>();
+
+    public Goods() {
+    }
+
+    public Goods(String name, String descr) {
+        this.name = name;
+        this.descr = descr;
+    }
 
     public Long getId() {
         return id;
@@ -43,4 +63,14 @@ public class Goods{
     public void setDescr(String descr) {
         this.descr = descr;
     }
+
+    public Set<Categories> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Categories> categories) {
+        this.categories = categories;
+    }
+
+
 }

d2fb1952df2d1cf0c05bdfe7ca3f9c7647bdeb5b
Ivanov Evgenii <ivanoff_john@mail.ru>
2018-08-22 04:57:23 +0300

    ManyToMany without controllers

--- src/main/java/com/SearchGoods/SearchGoodsApplication.java
+++ src/main/java/com/SearchGoods/SearchGoodsApplication.java
@@ -12,40 +12,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class SearchGoodsApplication implements CommandLineRunner {
-
-	@Autowired
-	private CategoriesRepository categoriesRepository;
-
-	@Autowired
-	private GoodsRepository goodsRepository;
+public class SearchGoodsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SearchGoodsApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		// Cleanup the tables
-		goodsRepository.deleteAllInBatch();
-		categoriesRepository.deleteAllInBatch();
-
-
-		Goods goods = new Goods("Java", "It's very common");
-
-		// Create two tags
-		Categories cat1 = new Categories("Book");
-		Categories cat2 = new Categories("Program");
-
-
-		// Add tag references in the post
-		goods.getCategories().add(cat1);
-		goods.getCategories().add(cat2);
-
-		// Add post reference in the tags
-		cat1.getGoods().add(goods);
-		cat2.getGoods().add(goods);
-
-		goodsRepository.save(goods);
 	}
 }
